aef54c345c1cb60f453e5ca4a8091f9dc43f5ff8
XtreamVox <145382798+XtreamVox@users.noreply.github.com>
2024-12-27 11:54:52 +0100

    Definición de constantes y de las bibliotecas necesarias

--- simul_ext.c
+++ simul_ext.c
@@ -1,3 +1,14 @@
 /*
 Simulador de ficheros tipo Linux
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#define SIZE_BLOQUE 512
+#define MAX_BLOQUES_PARTICION 100
+#define MAX_INODOS 24
+#define MAX_NUMS_BLOQUE_INODO 7
+#define LEN_NFICH 32
