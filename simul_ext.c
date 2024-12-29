0ae2341b5e75a3b697e285293d9c7ed1dd67afa8
XtreamVox <145382798+XtreamVox@users.noreply.github.com>
2024-12-29 17:26:01 +0100

    Corrección en las definiciones de las variables y de tipos en las llamadas a las funciones

--- simul_ext.c
+++ simul_ext.c
@@ -29,10 +29,10 @@ void eliminarSaltoLinea(char *cadena);
 
 int main()
 {
-	 char *comando[LONGITUD_COMANDO];
-	 char *orden[LONGITUD_COMANDO];
-	 char *argumento1[LONGITUD_COMANDO];
-	 char *argumento2[LONGITUD_COMANDO];
+	 char comando[LONGITUD_COMANDO];
+	 char orden[LONGITUD_COMANDO];
+	 char argumento1[LONGITUD_COMANDO];
+	 char argumento2[LONGITUD_COMANDO];
 	 
 	 int i,j;
 	 unsigned long int m;
@@ -53,9 +53,9 @@ int main()
     // Lectura del fichero y guardado de su contenido
      fread(&datosfich, SIZE_BLOQUE, MAX_BLOQUES_PARTICION, fent);        
      memcpy(&ext_superblock,(EXT_SIMPLE_SUPERBLOCK *)&datosfich[0], SIZE_BLOQUE);
-     memcpy(&directorio,(EXT_ENTRADA_DIR *)&datosfich[3], SIZE_BLOQUE);
      memcpy(&ext_bytemaps,(EXT_BLQ_INODOS *)&datosfich[1], SIZE_BLOQUE);
      memcpy(&ext_blq_inodos,(EXT_BLQ_INODOS *)&datosfich[2], SIZE_BLOQUE);
+     memcpy(&directorio,(EXT_ENTRADA_DIR *)&datosfich[3], SIZE_BLOQUE);
      memcpy(&memdatos,(EXT_DATOS *)&datosfich[4],MAX_BLOQUES_DATOS*SIZE_BLOQUE);
      
      // Buce de tratamiento de comandos
@@ -73,7 +73,7 @@ int main()
 
 	     if (strcmp(orden,"dir")==0) 
         {
-            Directorio(&directorio,&ext_blq_inodos);
+            Directorio(directorio,&ext_blq_inodos);
             continue;
         } 
         else if (strcmp(orden, "bytemaps") == 0)
@@ -134,12 +134,13 @@ int main()
             }
         }
          // Escritura de metadatos en comandos rename, remove, copy     
-         Grabarinodosydirectorio(&directorio,&ext_blq_inodos,fent);
+         Grabarinodosydirectorio(directorio,&ext_blq_inodos,fent);
          GrabarByteMaps(&ext_bytemaps,fent);
          GrabarSuperBloque(&ext_superblock,fent);
-         if (grabardatos)
-           GrabarDatos(&memdatos,fent);
-         grabardatos = 0;
+         if (grabardatos){
+           GrabarDatos(memdatos,fent);
+            grabardatos = 0;
+         }
          //Si el comando es salir se habrÃ¡n escrito todos los metadatos
          //faltan los datos y cerrar
          if (strcmp(orden,"salir")==0){
